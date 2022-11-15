19689d314c2b67e19b627e14effe66eef53f7d13
Sunpil Kim <ezweb28@gmail.com>
2022-11-15 00:30:19 -0500

    Initial commit

---
+++ src/main/java/com/skim/demo/demo/SpringBootJmsPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.skim.demo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJmsPublisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJmsPublisherApplication.class, args);
+    }
+
+}
